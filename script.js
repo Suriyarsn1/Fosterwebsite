e42582c96358a71e7aa8f02a27b92c379360dfa5
Suriyarsn1 <ssuriya1806@gmail.com>
2024-11-02 22:17:36 +0800

    newfile

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var mySwiper
+mySwiper = new Swiper('.swiper-container', {
+   
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+const phoneInputField = document.querySelector("#mob");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+
